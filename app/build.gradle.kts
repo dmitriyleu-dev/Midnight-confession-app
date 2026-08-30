e795440e4ae63da50bd076e21c78405ec44a057a
dmitriyleu-dev <dmitriyleu@gmail.com>
2026-08-30 19:20:26 +0300

    Create build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    id("com.android.application") version "8.7.3"
+}
+
+android {
+    namespace = "com.midnightconfession.host"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.midnightconfession.host"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "0.1"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
